6a0a30578e01ecf2029da28a27a2ec75a3e45d4e
Chris Taylor <chrismtaylor1998@gmail.com>
2020-08-08 13:08:45 -0400

    working dynamic robo cards

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@
 import React from 'react'; // this is the robot that handles the view (can be anything)
 import ReactDOM from 'react-dom'; // specifically for websites
 import './index.css'; // can add css for each component
-import Hello from './Hello.js';
+import Card from './Card.js';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import {robots} from './robots';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Hello greeting={'Hello ' + 'React Ninja'}/>
+    <div>
+      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+    </div>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
