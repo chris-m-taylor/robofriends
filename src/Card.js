d71b70dfc626218b129bb7d72e17896587449b5e
Chris Taylor <chrismtaylor1998@gmail.com>
2020-08-09 19:25:37 -0400

    Added cardList class

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ const Card = ({name, email, id}) => { //destructuring
     
     return (
 
-        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+        <div className="tc bg-light-green dib br3 pa3 ma2 bw2 grow shadow-5">
             <img alt="robots" src={`https://robohash.org/${id}?size=200x200`}/>
             <div>
                 <h2>{name}</h2>
--- src/index.js
+++ src/index.js
@@ -3,19 +3,14 @@
 import React from 'react'; // this is the robot that handles the view (can be anything)
 import ReactDOM from 'react-dom'; // specifically for websites
 import './index.css'; // can add css for each component
-import Card from './Card.js';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import CardList from './CardList';
 import {robots} from './robots';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-    </div>
-    
+    <CardList robots={robots}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
